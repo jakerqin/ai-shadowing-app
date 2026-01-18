de64067249a998905531405a5806a712dc89cde4
Jax <qljmeinan@163.com>
2026-01-18 19:23:00 +0800

    feat(routes): add learning plan routes
    - /create-plan: user input and plan generation
    - /plan-preview: preview generated plan before saving
    - /my-plan: view and manage learning plan progress

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Generator from './pages/Generator'
 import Result from './pages/Result'
 import Notebook from './pages/Notebook'
 import Settings from './pages/Settings'
+import CreatePlan from './pages/CreatePlan'
+import PlanPreview from './pages/PlanPreview'
+import MyPlan from './pages/MyPlan'
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
             <Route path="/result" element={<Result />} />
             <Route path="/notebook" element={<Notebook />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/create-plan" element={<CreatePlan />} />
+            <Route path="/plan-preview" element={<PlanPreview />} />
+            <Route path="/my-plan" element={<MyPlan />} />
           </Routes>
         </div>
       </Router>
